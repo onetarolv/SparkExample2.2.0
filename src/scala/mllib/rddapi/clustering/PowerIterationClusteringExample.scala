d1d0a3ecbb4658d5e5cba9c226aa88fa66f7a6ea
onetarolv <lw_engry@outlook.com>
2017-11-29 16:55:18 +0800

    push PowerIterationClusteringExample 2017-11-19

--- src/scala/mllib/rddapi/clustering/PowerIterationClusteringExample.scala
+++ src/scala/mllib/rddapi/clustering/PowerIterationClusteringExample.scala
@@ -1,7 +1,7 @@
 package mllib.rddapi.clustering
 
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.SparkContext
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.mllib.clustering.PowerIterationClustering
@@ -10,16 +10,43 @@ import org.apache.spark.mllib.clustering.PowerIterationClustering
  * Created by engry on 2017/11/28.
  */
 object PowerIterationClusteringExample {
+  case class Params(
+                   k: Int = 2,
+                   numPoints: Int = 10,
+                   maxIterations: Int = 15
+                     ) extends AbstractParams[Params]
   def main(args: Array[String]) {
     Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
-    System.setProperty("hadoop.home.dir", "D:\\hadoop-2.7.4");
+    System.setProperty("hadoop.home.dir", "D:\\envpath\\hadoop-2.6.4");
     val spark = SparkSession.builder()
       .appName("PICExample")
       .master("local")
       .getOrCreate()
+
+    val k = 10
+    val numPoints = 10
+    val sc = spark.sparkContext
+    val circlesRdd = generateCirclesRdd(sc, k, numPoints)
+    val model = new PowerIterationClustering()
+      .setK(k)
+      .setMaxIterations(maxIterations = 80)
+      .setInitializationMode("degree")
+      .run(circlesRdd)
+
+    val clusters = model.assignments.collect()
+      .groupBy(_.cluster).mapValues(_.map(_.id))
+    val assignments = clusters.toList.sortBy { case (k, v) => v.length }
+    val assignmentsStr = assignments
+      .map { case (k, v) =>
+        s"$k -> ${v.sorted.mkString("[", ",", "]")}"
+      }mkString(",")
+
+    val sizesStr = assignments.map {
+      _._2.length
+    }.sorted.mkString("(", ",", ")")
+    println(s"Cluster assignments: $assignmentsStr\ncluster sizes: $sizesStr")
+    spark.stop()
   }
-
-
 
   def generateCircle(radius: Double, n: Int): Seq[(Double, Double)] = {
     Seq.tabulate(n) { i =>
